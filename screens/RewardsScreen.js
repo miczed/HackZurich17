afd81dd796c03fda13d58386c552f82ef644878a
Martin Juan José Bucher <manibucher@gmx.net>
2017-09-16 23:33:46 +0200

    Add Reward View

--- screens/RewardsScreen.js
+++ screens/RewardsScreen.js
@@ -1,23 +1,130 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Diamond from "../components/diamond";
+import { StyleSheet, Text, Image, View, FlatList } from 'react-native';
 
 export default class RewardsScreen extends React.Component {
-    render() {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            data: []
+        };
+    }
+
+    componentWillMount(){
+        this.addMockRewardsData();
+    }
+
+    addMockRewardsData(){
+
+        const award1 = { title: "The Check-In Beginner", peakPoints: 1500, currentScore: 2, maxScore: 5, text: "Check-Ins", image: require('../img/anker.png') };
+        const award2 = { title: "PeakPoint Hunter", peakPoints: 2000, currentScore: 23, maxScore: 100, text: "PeakPoints", image: require('../img/bolt.png') };
+        const award3 = { title: "Obi-Wan Kenobi", peakPoints: 3500, currentScore: 0, maxScore: 30, text: "Check-Ins", image: require('../img/jet.png') };
+
+        let data = [];
+        data.push(award1);
+        data.push(award2);
+        data.push(award3);
+
+        this.setState({
+            data: data,
+        });
+
+    }
+
+    renderItem({ item, index }) {
         return (
-            <View style={styles.container}>
-                <Text>Rewards Screen Baby!!!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
+            <View style={[styles.reward, styles.card]}>
+
+                <Image source={item.image} style={styles.rewardImage} />
+
+                <View style={styles.rewardDescription} >
+                    <Text style={styles.rewardDescriptionTitle}>{item.title}</Text>
+                    <Text style={styles.rewardDescriptionText}>{item.currentScore} / {item.maxScore} {item.text}</Text>
+                </View>
+
+                <Diamond peakPoints={item.peakPoints} style={styles.diamond} />
+
             </View>
         );
     }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+            <FlatList
+                data={this.state.data}
+                renderItem={this.renderItem}
+                keyExtractor={(item, index) => index}
+                style={styles.rewards}
+            />
+
+            </View>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ff00ff',
-        alignItems: 'center',
+        backgroundColor: '#F9F9FB',
+        alignItems: 'flex-start',
         justifyContent: 'center',
     },
+
+    rewards: {
+        flex: 1,
+        alignSelf: 'stretch',
+    },
+
+    reward: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    card: {
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3},
+        shadowOpacity: 0.05,
+        shadowRadius: 15,
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        alignSelf: 'stretch',
+    },
+
+    rewardImage: {
+        height: 40,
+        width: 40,
+        marginRight: 20
+    },
+
+    rewardDescription: {
+        flexDirection: 'column',
+    },
+
+    rewardDescriptionTitle: {
+        fontSize: 14,
+        fontWeight: '500',
+        marginBottom: 3,
+        color: '#2b2b2b',
+    },
+
+    rewardDescriptionText: {
+        color: '#828282',
+        fontSize: 12,
+    },
+
+    diamond: {
+        marginLeft: 'auto',
+    }
+
 });
