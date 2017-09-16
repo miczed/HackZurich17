38b006836f8276c798fcff0605d000d8f2c84650
miczed <mighty.miczed@gmail.com>
2017-09-16 19:17:45 +0200

    Rudimental implementation of time table

--- screens/ConnectionsScreen.js
+++ screens/ConnectionsScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView,View } from 'react-native';
 
 export default class ConnectionsScreen extends React.Component {
+    constructor(props) {
+        super();
+    }
+    displayConnection(connection) {
+        return (
+            <View style={styles.card}>
+                <Text>Departure: { connection.from.departure }</Text>
+                <Text>Arrival: { connection.to.arrival }</Text>
+                <Text>Duration: { connection.duration }</Text>
+            </View>
+        )
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>Connections Screen Baby!!!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
+                <Text style={styles.heading}>From <Text style={styles.bold}>{ this.props.data.from.name }</Text></Text>
+                <Text style={styles.heading}>To <Text style={styles.bold}>{ this.props.data.to.name  }</Text></Text>
+                <ScrollView style={styles.scrollView}>
+                    { this.props.data.connections.map(this.displayConnection) }
+                </ScrollView>
             </View>
         );
     }
@@ -16,8 +30,33 @@ export default class ConnectionsScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#0000ff',
+        backgroundColor: '#F9F9FB',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20,
     },
+    heading: {
+        fontSize: 18,
+        textAlign: 'left',
+        alignSelf: 'flex-start',
+    },
+    scrollView: {
+      alignSelf: 'stretch',
+    },
+    card: {
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3},
+        shadowOpacity: 0.05,
+        shadowRadius: 15,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        marginTop: 10,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+    },
+    bold: {
+        fontWeight: 'bold',
+    }
 });
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image,AlertIOS } from 'react-native';
 import Button from 'apsl-react-native-button'
 import NotificationsIOS from 'react-native-notifications';
+import Data from '../lib/data';
 
 export default class SearchScreen extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+                from: null,
+                to: null,
+                loading: false,
+        };
+    }
     _onPressButton = () => {
-        this.props.navigator.push({
-            screen: 'peakpass.ConnectionsScreen', // unique ID registered with Navigation.registerScreen
-            title: "Connections", // navigation bar title of the pushed screen (optional)
-            //titleImage: require('../../img/my_image.png'), // iOS only. navigation bar title image instead of the title text of the pushed screen (optional)
-            //passProps: {}, // Object that will be passed as props to the pushed screen (optional)
-            animated: true, // does the push have transition animation or does it happen immediately (optional)
-            animationType: 'slide', // 'fade' (for both) / 'slide-horizontal' (for android) does the push have different transition animation (optional)
-            //backButtonTitle: undefined, // override the back button title (optional)
-            //backButtonHidden: false, // hide the back button altogether (optional)
-            //navigatorStyle: {}, // override the navigator style for the pushed screen (optional)
-            //navigatorButtons: {} // override the nav buttons for the pushed screen (optional)
+        this.setState({loading: true});
+        Data.handleSearch(this.state.from,this.state.to).then((response) => {
+            this.setState({loading: false});
+            this.props.navigator.push({
+                screen: 'peakpass.ConnectionsScreen', // unique ID registered with Navigation.registerScreen
+                title: "Connections", // navigation bar title of the pushed screen (optional)
+                //titleImage: require('../../img/my_image.png'), // iOS only. navigation bar title image instead of the title text of the pushed screen (optional)
+                passProps: { data: response.data }, // Object that will be passed as props to the pushed screen (optional)
+                animated: true, // does the push have transition animation or does it happen immediately (optional)
+                animationType: 'slide', // 'fade' (for both) / 'slide-horizontal' (for android) does the push have different transition animation (optional)
+                //backButtonTitle: undefined, // override the back button title (optional)
+                //backButtonHidden: false, // hide the back button altogether (optional)
+                //navigatorStyle: {}, // override the navigator style for the pushed screen (optional)
+                //navigatorButtons: {} // override the nav buttons for the pushed screen (optional)
+            });
+        }).catch((error) => {
+            if (error.response) {
+                // The request was made, but the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else {
+                console.log(error);
+            }
+
+            this.setState({loading: false});
+            AlertIOS.alert("Couldn't get connections from the API.","Make sure that you're connected to the internet and please try again.");
         });
+
     }
 
     _triggerNotif = () => {
@@ -45,9 +72,10 @@ export default class SearchScreen extends React.Component {
                     placeholder="Bern"
                     onChangeText={(to) => this.setState({to})}
                 />
-                <Button onPress={this._triggerNotif} style={styles.submitButton} textStyle={{fontSize: 18}}>
+                <Button isLoading={this.state.loading} isDisabled={!this.state.from || !this.state.to} onPress={this._onPressButton} style={styles.submitButton} textStyle={{fontSize: 18}}>
                     GO
                 </Button>
+                <Image source={require('../img/brands.png')} style={styles.brands} />
             </View>
         );
     }
@@ -67,12 +95,17 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
         borderRadius: 8,
         backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3},
+        shadowOpacity: 0.05,
+        shadowRadius: 15,
     },
     formLabel: {
         textAlign: 'left',
         alignSelf: 'stretch',
         paddingBottom: 10,
         paddingTop: 10,
+        color: '#828282',
     },
     submitButton: {
         borderRadius: 8,
@@ -81,5 +114,10 @@ const styles = StyleSheet.create({
         borderWidth: 0,
     },
     submitButtonText: {
+    },
+    brands: {
+        alignSelf: 'flex-start',
+        marginTop: 'auto',
     }
+
 });
